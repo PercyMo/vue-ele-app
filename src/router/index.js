1aeeacb2b2eac600d4c048fdeab470404466ce53
maobangxin <1418968663@qq.com>
2018-03-29 19:06:36 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,50 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import {routerMode} from '../config/env'
 
 Vue.use(Router)
 
+const Msite = r => require.ensure([], () => r(require('views/msite/msite')), 'Msite')
+
+const Search = r => require.ensure([], () => r(require('views/search/search')), 'Search')
+
+const Order = r => require.ensure([], () => r(require('views/order/order')), 'Order')
+
+const Profile = r => require.ensure([], () => r(require('views/profile/profile')), 'Profile')
+
+const Find = r => require.ensure([], () => r(require('views/find/find')), 'Find')
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    mode: routerMode,
+    routes: [
+        {
+            path: '/',
+            component: Msite //首屏重定向
+        },
+        // 所有商铺列表页
+        {
+            path: '/msite',
+            component: Msite
+        },
+        // 搜索页
+        {
+            path: '/search',
+            component: Search
+        },
+        // 订单列表页
+        {
+            path: '/order',
+            component: Order
+        },
+        // 个人信息页
+        {
+            path: '/profile',
+            component: Profile
+        },
+        // 发现页
+        {
+            path: '/find',
+            component: Find
+        }
+    ]
 })
---
+++ src/views/msite/msite.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        ssite
+        <foot-guide></foot-guide>
+    </div>
+</template>
+
+<script>
+    import FootGuide from 'components/footer/footGuide'
+
+    export default {
+        components: {
+            FootGuide
+        }
+    }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
