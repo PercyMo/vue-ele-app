57f3b05cf812af6139e7d5a6014b48651b62db51
maobangxin <1418968663@qq.com>
2018-04-09 18:08:51 +0800

    1

--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -9,17 +9,27 @@
             </div>
         </form>
         <header v-if="historyTitle" class="pois-search-history">搜索历史</header>
+        <ul class="getpois-list">
+            <li class="getpois-item" v-for="(item, index) in placelist" @click="nextpage(index, item.geohash)" :key="index">
+                <h4 class="posi-name ellipsis">{{item.name}}</h4>
+                <p class="posi-address ellipsis">{{item.address}}</p>
+            </li>
+        </ul>
+        <footer v-if="historyTitle && placelist.length" class="clear-all-history">清空所有</footer>
+        <div class="search-none-place" v-if="placeNone">很抱歉！无搜索结果</div>
     </div>
 </template>
 
 <script>
-    import {searchplace} from '../../service/getData'
+    import {searchplace} from 'service/getData'
 
     export default {
         data() {
             return {
                 inputVaule: '',
-                historyTitle: true
+                placelist: [],
+                historyTitle: true,
+                placeNone: false
             }
         },
         computed: {
@@ -32,9 +42,14 @@
             postpois() {
                 if (this.inputVaule) {
                     searchplace(this.cityid, this.inputVaule).then((res) => {
-                        console.log(res)
+                        this.historyTitle = false
+                        this.placelist = res
+                        this.placeNone = res.length ? false : true
                     })
                 }
+            },
+            nextpage(index, geohash) {
+
             }
         },
         components: {
@@ -76,4 +91,29 @@
         line-height .8rem
         border-top 1px solid $bc
         border-bottom 1px solid $bc
+    .getpois-list
+        background-color #fff
+        border-top 1px solid $bc
+        .getpois-item
+            margin 0 auto
+            padding-top .4rem
+            border-bottom 1px solid $bc
+            .posi-name
+                margin 0 auto
+                width 90%
+                color #333
+                font-size .65rem
+                font-weight bold
+            .posi-address
+                margin 0 auto .4rem
+                width 90%
+                color #999
+                font-size .45rem
+    .search-none-place
+        margin 0 auto
+        color #333
+        font-size .65rem
+        line-height 1.75rem
+        text-indent .5rem
+        background-color #fff
 </style>
